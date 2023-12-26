a1a67340c4c3e7661514d64503069b4187873a8a
Swami <anilsanapathi9@gmail.com>
2023-12-26 12:48:20 +0530

    refactor: update Prettier configuration and remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,9 @@
 import { Component } from '@angular/core';
-import { EChartsOption } from 'echarts';
 import { ChartCmp } from './charts/chart.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     template: ` <app-chart /> `,
-    styles: `
-    .demo-chart {
-        height: 600px;
-    }
-`,
     imports: [ChartCmp]
 })
-export class AppComponent {
-    title = 'charts';
-    chartOption: EChartsOption = {
-        xAxis: {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-            type: 'value'
-        },
-        series: [
-            {
-                data: [820, 932, 901, 934, 1290, 1330, 1320],
-                type: 'line'
-            }
-        ]
-    };
-}
+export class AppComponent {}
